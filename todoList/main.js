9687977aa21895690f6eb8cad6051a6715d8a265
Diwakar Kumar <51357882+Diwakarkumar3096@users.noreply.github.com>
2023-03-15 18:46:19 +0530

    TodoList

---
+++ todoList/main.js
@@ -0,0 +1,10 @@
+function addMore(){
+    document.getElementById('error').innerHTML="";
+    var name=document.getElementById('name').value;
+    if(name ==''){
+         document.getElementById('error').innerHTML="please enter value";
+    }
+    else{
+
+    }
+}
